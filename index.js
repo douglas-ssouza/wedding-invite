c699f25ca4b5c65f0deeeff42d828f402b98778d
douglas-ssouza <douglas.souza@ringa.com.br>
2024-03-11 16:24:20 -0300

    adds waze button

--- index.js
+++ index.js
@@ -12,4 +12,14 @@ function openGoogleMapsDirections() {
   
 }
 
+function openWazeRoute() {
+  const destinationLat = -23.336816787719727;
+  const destinationLng = -46.22216796875;
+
+  const wazeUrl = 'https://waze.com/ul?q=' + destinationLat + ',' + destinationLng + '&navigate=yes';
+
+  window.open(wazeUrl, '_blank');
+}
+
 document.getElementById("showRouteBtn").addEventListener("click", openGoogleMapsDirections);
+document.getElementById("wazeBtn").addEventListener("click", openWazeRoute);
