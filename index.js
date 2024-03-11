4b59c54e0aebdda58c5f3b3fc2b9b388916b224f
douglas-ssouza <douglas.souza@ringa.com.br>
2024-03-11 16:55:10 -0300

    adjusts functions

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
+const destinationLat = -23.336816787719727;
+const destinationLng = -46.22216796875;
+
 function openGoogleMapsDirections() {
-  const destinationCoords = "-23.336816787719727,-46.22216796875";
+  const destinationCoords = `${destinationLat},${destinationLng}`;
 
   const isMobile = /Android/i.test(navigator.userAgent);
 
   if (isMobile) {
-    const mapsMobileUrl = "geo:?daddr=" + encodeURIComponent(destinationCoords);
+    const mapsMobileUrl = 'geo:?daddr=' + encodeURIComponent(destinationCoords);
     window.open(mapsMobileUrl);
   } else {
-    window.open("https://maps.google.com/maps?ll=-23.336552,-46.222638&z=16&t=m&hl=pt-BR&gl=US&mapclient=apiv3&cid=17848247453708602673");
+    window.open('https://maps.google.com/maps?ll=-23.336552,-46.222638&z=16&t=m&hl=pt-BR&gl=US&mapclient=apiv3&cid=17848247453708602673');
   }
-  
 }
 
 function openWazeRoute() {
-  const destinationLat = -23.336816787719727;
-  const destinationLng = -46.22216796875;
-
   const wazeUrl = 'https://waze.com/ul?q=' + destinationLat + ',' + destinationLng + '&navigate=yes';
-
   window.open(wazeUrl, '_blank');
 }
 
-document.getElementById("mapsBtn").addEventListener("click", openGoogleMapsDirections);
-document.getElementById("wazeBtn").addEventListener("click", openWazeRoute);
+document.getElementById('mapsBtn').addEventListener('click', openGoogleMapsDirections);
+document.getElementById('wazeBtn').addEventListener('click', openWazeRoute);
