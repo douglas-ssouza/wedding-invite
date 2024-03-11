05212690b1e42b9365dcb6369f02a94afa7dc2d0
douglas-ssouza <douglas.souza@ringa.com.br>
2024-03-11 16:15:29 -0300

    removes geolocation check

--- index.js
+++ index.js
@@ -7,20 +7,7 @@ function openGoogleMapsDirections() {
     const mapsMobileUrl = "geo:?daddr=" + encodeURIComponent(destinationCoords);
     window.open(mapsMobileUrl);
   } else {
-    if ("geolocation" in navigator) {
-      window.open("https://maps.google.com/maps?ll=-23.336552,-46.222638&z=16&t=m&hl=pt-BR&gl=US&mapclient=apiv3&cid=17848247453708602673");
-      // if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
-      //   window.open("https://maps.google.com/maps?ll=-23.336552,-46.222638&z=16&t=m&hl=pt-BR&gl=US&mapclient=apiv3&cid=17848247453708602673");
-      // } else {
-      //   navigator.geolocation.getCurrentPosition(function(position) {
-      //     const userCoords = position.coords.latitude + "," + position.coords.longitude;
-      //     const mapsDesktopUrl = "https://www.google.com/maps/dir/?api=1&origin=" + encodeURIComponent(userCoords) + "&destination=" + encodeURIComponent(destinationCoords);
-      //     window.open(mapsDesktopUrl, "_blank");
-      //   });
-      // }
-    } else {
-      alert("Geolocation is not supported by your browser.");
-    }
+    window.open("https://maps.google.com/maps?ll=-23.336552,-46.222638&z=16&t=m&hl=pt-BR&gl=US&mapclient=apiv3&cid=17848247453708602673");
   }
   
 }
