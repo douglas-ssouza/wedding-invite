369ba347596a7454ddcc00e2b1432ebc6759acc8
douglas-ssouza <douglas.souza@ringa.com.br>
2024-03-11 16:09:45 -0300

    fixes iphone condition

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ function openGoogleMapsDirections() {
   const isMobile = /Android/i.test(navigator.userAgent);
 
   if (isMobile) {
-    const mapsMobileUrl = /iPhone|iPad|iPod/i.test(navigator.userAgent)
-      ? "https://maps.google.com/maps?ll=-23.336552,-46.222638&z=16&t=m&hl=pt-BR&gl=US&mapclient=apiv3&cid=17848247453708602673"
-      : "geo:?daddr=" + encodeURIComponent(destinationCoords);
+    const mapsMobileUrl = "geo:?daddr=" + encodeURIComponent(destinationCoords);
     window.open(mapsMobileUrl);
   } else {
-    if (!"geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        const userCoords = position.coords.latitude + "," + position.coords.longitude;
-        const mapsDesktopUrl = "https://www.google.com/maps/dir/?api=1&origin=" + encodeURIComponent(userCoords) + "&destination=" + encodeURIComponent(destinationCoords);
-        window.open(mapsDesktopUrl, "_blank");
-      });
+    if ("geolocation" in navigator) {
+      if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+        window.open("https://maps.google.com/maps?ll=-23.336552,-46.222638&z=16&t=m&hl=pt-BR&gl=US&mapclient=apiv3&cid=17848247453708602673", "_blank");
+      } else {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          const userCoords = position.coords.latitude + "," + position.coords.longitude;
+          const mapsDesktopUrl = "https://www.google.com/maps/dir/?api=1&origin=" + encodeURIComponent(userCoords) + "&destination=" + encodeURIComponent(destinationCoords);
+          window.open(mapsDesktopUrl, "_blank");
+        });
+      }
     } else {
       alert("Geolocation is not supported by your browser.");
     }
