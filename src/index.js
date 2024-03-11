fe54ef1d62ad8f11ff6956d2bd3c476456a12613
douglas-ssouza <douglas.souza@ringa.com.br>
2024-03-11 15:02:06 -0300

    moves script to a separate fil

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function openGoogleMapsDirections() {
+  const destinationCoords = "-23.336816787719727,-46.22216796875";
+  
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      const userCoords = position.coords.latitude + "," + position.coords.longitude;
+      const mapsUrl = "https://www.google.com/maps/dir/?api=1&origin=" + encodeURIComponent(userCoords) + "&destination=" + encodeURIComponent(destinationCoords);
+      window.open(mapsUrl, "_blank");
+    });
+  } else {
+    alert("Geolocation is not supported by your browser.");
+  }
+}
 
+document.getElementById("showRouteBtn").addEventListener("click", openGoogleMapsDirections);
